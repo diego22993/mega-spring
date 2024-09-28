e72573d836622f7ad3cadaa0623bc72fae34ed6a
diego22993 <diegonicolasheredialigorria@gmail.com>
2024-09-28 00:42:10 -0300

    Implementamos metodo precario de listado de usuarios getUsuarios

--- springboot-app/springboot-app/src/main/java/com/diegola/springboot/app/controllers/UsuarioController.java
+++ springboot-app/springboot-app/src/main/java/com/diegola/springboot/app/controllers/UsuarioController.java
@@ -17,4 +17,26 @@ public class UsuarioController {
         return u1.toString();
     }
 
+    @RequestMapping("/usuarios")
+    public ArrayList<Usuario> getUsuarios(){
+        ArrayList<Usuario> listado = new ArrayList<>();
+        Usuario u1 = new Usuario();
+        Usuario u2 = new Usuario();
+        Usuario u3 = new Usuario();
+        u1.setId(100L);
+        u1.setNombre("Rivaldo");
+        u1.setApellido("Morales");
+        u2.setId(200L);
+        u2.setNombre("Cesar");
+        u2.setApellido("Lovera");
+        u3.setId(300L);
+        u3.setNombre("Diogo");
+        u3.setApellido("Heratia");
+
+        listado.add(u1);
+        listado.add(u2);
+        listado.add(u3);
+
+        return listado;
+    }
 }
