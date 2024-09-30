ee3914a01521ceae709390a8ca1b4d7dca031087
diego22993 <diegonicolasheredialigorria@gmail.com>
2024-09-30 01:16:02 -0300

    Añadimos Notaciones de Lombok en clase Customer

--- springboot-app/springboot-app/src/main/java/com/diegola/springboot/app/models/Customer.java
+++ springboot-app/springboot-app/src/main/java/com/diegola/springboot/app/models/Customer.java
@@ -1,35 +1,17 @@
 package com.diegola.springboot.app.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Customer {
-    @Getter @Setter
     private String name;
-    @Getter @Setter
     private String telphone;
-    @Getter @Setter
     private String email;
-    @Getter @Setter
     private String address;
 
-    public Customer(){
-    }
-
-    public Customer(String name, String telphone, String email, String address) {
-        this.name = name;
-        this.telphone = telphone;
-        this.email = email;
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "name='" + name + '\'' +
-                ", telphone=" + telphone +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
